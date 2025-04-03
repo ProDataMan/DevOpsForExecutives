c0918405d124343506d8ed37e5a9e277201e83b8
ProDataMan <antoine.victor@prodataman.com>
2025-04-03 14:26:34 -0700

    update folder structure

--- Week8/test.js
+++ Week8/test.js
@@ -1,4 +1,4 @@
-   const app = require('./index');
+   const app = require('../index');
    const assert = require('assert');
    assert.ok(true, 'App loads'); // Simple pass
    console.log('Tests passed');
